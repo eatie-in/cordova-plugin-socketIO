093919937c158683c276e70fb393331c7859aaa3
vishnu <vvr947@gmail.com>
2021-02-13 18:33:48 +0530

    plugin js file refactored

--- www/socketIOPlugin.js
+++ www/socketIOPlugin.js
@@ -35,50 +35,8 @@ socketIOPlugin.prototype.emit = (options) =>
   });
 
 socketIOPlugin.prototype.onMessage = (successCallback,errorCallback) => {
-  // // socketIOPlugin.prototype.onMessageReceived = callback;
-  // return new Promise((resolve, reject) => {
-  //   // socketIOPlugin.prototype.onMessageReceived = resolve;
-  //   exec(resolve, reject, PLUGIN_NAME, "onMessage", []);
-  // });
-
   exec(successCallback, errorCallback, PLUGIN_NAME, "onMessage", []);
-};
-
-socketIOPlugin.prototype.onMessageReceived = (payload) => {
-  console.log("[RECEIVED]", payload);
 };
 
 module.exports = new socketIOPlugin();
 
-// module.exports = {
-//   test: function (text, errorCallback, successCallback) {
-//     exec(successCallback, errorCallback, PLUGIN_NAME, "test", [text]);
-//   },
-
-//   connect: (options, successCallback, errorCallback) => {
-//     exec(successCallback, errorCallback, PLUGIN_NAME, "connect", [options]);
-//   },
-
-//   addListener: (options, successCallback, errorCallback) => {
-//     exec(successCallback, errorCallback, PLUGIN_NAME, "addListener", [options]);
-//   },
-
-//   removeListener: (options, successCallback, errorCallback) => {
-//     exec(successCallback, errorCallback, PLUGIN_NAME, "addListener", [options]);
-//   },
-
-//   disconnect: (options, successCallback, errorCallback) => {
-//     exec(successCallback, errorCallback, PLUGIN_NAME, "connect", [options]);
-//   },
-//   onMessage: (callback) => {
-//     this.onMessageReceived = callback;
-//     console.log(callback, "red", this.onMessageReceived);
-//   },
-//   onMessageReceived: (payload) => {
-//     console.log("messageReceived");
-//     console.log(payload);
-//   },
-//   emit: (options, successCallback, errorCallback) => {
-//     exec(successCallback, errorCallback, PLUGIN_NAME, "emit", [options]);
-//   },
-// };
